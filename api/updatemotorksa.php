b88c9848cc17e33c76f0ad7e2ae521337cbed733
Ziyad <62690285+ZiyadAhmed910@users.noreply.github.com>
2021-10-13 12:22:56 +0530

    Add files via upload

---
+++ api/updatemotorksa.php
@@ -0,0 +1,42 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+//Creating Array for JSON response
+$response = array();
+
+// Check if we got the field from the user
+if (isset($_GET['id']) && isset($_GET['status'])) {
+
+    $id = $_GET['id'];
+    $status = $_GET['status'];
+
+
+    // Include data base connect class
+    $filepath = realpath(dirname(__FILE__));
+    require_once($filepath . "/db_connect.php");
+
+    // Connecting to database
+
+    // Fire SQL query to update LED status data by id
+    $result = mysqli_query($conn, "UPDATE motorstatus SET status= '$status' WHERE id = '$id'");
+
+    // Check for succesfull execution of query and no results found
+    if ($result) {
+        // successfully updation of LED status (status)
+        $response["success"] = 1;
+        $response["message"] = "MOTOR Status successfully updated.";
+
+        // Show JSON response
+        echo json_encode($response);
+    } else {
+    }
+} else {
+    // If required parameter is missing
+    $response["success"] = 0;
+    $response["message"] = "Parameter(s) are missing. Please check the request";
+
+    // Show JSON response
+    echo json_encode($response);
+}
